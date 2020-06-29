55a01a894e3bb44083d9fd12672dd527c14d3f7b
LoneStar <ernieurzua@gmail.com>
2020-06-29 11:09:05 -0700

    added build query function

--- script.js
+++ script.js
@@ -2,23 +2,36 @@ $(document).ready(function () {
 
     // function to clear the fields
     function clear() {
-        $("#article-section").empty();
+        $("#forecast").empty();
     }
 
-    
+    /**
+ * pulls information from the form and build the query URL
+ * @returns {string} URL for NYT API based on form inputs
+ */
     function buildQueryURL() {
         // queryURL is the url we'll use to query the API
-        var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?";
-
+        var queryURL = "api.openweathermap.org/data/2.5/weather?";
+        //http://api.openweathermap.org/data/2.5/weather?q=los%20angeles&appid=b2d4239aa3e819b8680cdea4c57fe90d
         // Begin building an object to contain our API call's query parameters
         // Set the API key
-        var query = { "api-key": "b2d4239aa3e819b8680cdea4c57fe90d" };
+        var queryParams = { "appid": "b2d4239aa3e819b8680cdea4c57fe90d" };
+
+        queryParams.q = $("#search")
+            .val()
+            .trim();
+
+        // Logging the URL so we have access to it for troubleshooting
+        console.log("---------------\nURL: " + queryURL + "\n---------------");
+        console.log(queryURL + $.param(queryParams));
+        return queryURL + $.param(queryParams);
+
     }
 
 
-    $("#run-search").on("click", function (event) {
+    $("#search").on("click", function (event) {
         // This line allows us to take advantage of the HTML "submit" property
-
+        console.log(event);
         //prevents reloading of page by pressing enter
         event.preventDefault();
 
@@ -36,7 +49,18 @@ $(document).ready(function () {
         }).then(updatePage);
     });
 
+
+    /**
+ * takes API data (JSON/object) and turns it into elements on the page
+ * @param {object} OpenWeatherData - object containing NYT API data
+ */
+    function updatePage() {
+        // Log the openweatherdata to console, where it will show up as an object
+  console.log(OpenWeatherData);
+  console.log("------------------------------------");
+    }
+
     //  .on("click") function associated with the clear button
-    $("#clear-all").on("click", clear);
+    $("#forecast").on("click", clear);
 
 });
