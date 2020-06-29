8ce1565687fc2d4f2998ba49940e491774f2e095
ErnestUrzua <ernieurzua@gmail.com>
2020-06-29 12:11:49 -0700

    changed search area, fixed object return code

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
 
+
     // function to clear the fields
     function clear() {
         $("#forecast").empty();
@@ -9,33 +10,38 @@ $(document).ready(function () {
  * pulls information from the form and build the query URL
  * @returns {string} URL for NYT API based on form inputs
  */
+
+    //WORKS
     function buildQueryURL() {
         // queryURL is the url we'll use to query the API
-        var queryURL = "api.openweathermap.org/data/2.5/weather?";
+        var queryURL = "https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?";
         //http://api.openweathermap.org/data/2.5/weather?q=los%20angeles&appid=b2d4239aa3e819b8680cdea4c57fe90d
         // Begin building an object to contain our API call's query parameters
         // Set the API key
         var queryParams = { "appid": "b2d4239aa3e819b8680cdea4c57fe90d" };
 
-        queryParams.q = $("#search")
-            .val()
-            .trim();
+        queryURL.q = $("#searchText");
+
+        var queryText = $("#searchText")
+            .val().trim();
 
         // Logging the URL so we have access to it for troubleshooting
         console.log("---------------\nURL: " + queryURL + "\n---------------");
-        console.log(queryURL + $.param(queryParams));
-        return queryURL + $.param(queryParams);
+        console.log(queryURL + "q=" + queryText + "&" + $.param(queryParams));
+        return queryURL + "q=" + queryText + "&" + $.param(queryParams);
 
     }
 
-
-    $("#search").on("click", function (event) {
+    //WORKS paritally
+    // works on click but not on return or enter.
+    //searchs the city typed in the box by pressing enter
+    $("#searchText").on("click", function (event) {
         // This line allows us to take advantage of the HTML "submit" property
         console.log(event);
         //prevents reloading of page by pressing enter
         event.preventDefault();
 
-        // clear the forecast area
+        // clear the forecast area to populate a new one
         clear();
 
         // Build the query URL for the ajax request to the Open weather API
@@ -54,12 +60,14 @@ $(document).ready(function () {
  * takes API data (JSON/object) and turns it into elements on the page
  * @param {object} OpenWeatherData - object containing NYT API data
  */
-    function updatePage() {
+    //WORKS
+    function updatePage(OpenWeatherData) {
         // Log the openweatherdata to console, where it will show up as an object
-  console.log(OpenWeatherData);
-  console.log("------------------------------------");
+        console.log(OpenWeatherData);
+        console.log("------------------------------------");
     }
 
+    //UNTESTED
     //  .on("click") function associated with the clear button
     $("#forecast").on("click", clear);
 
